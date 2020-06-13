4d127c7f05a85e4b1fdbe006fd9afd651624c3b1
Weydson <weydson@github.com>
2020-06-12 23:13:44 -0300

    upload folder4

--- config/multer.ts
+++ config/multer.ts
@@ -3,7 +3,7 @@ import path = require('path');
 import crypto from 'crypto';
 export default {
     storage:multer.diskStorage({
-        destination: '/app/public/uploads',
+        destination: path.resolve(__dirname, '..', '..', 'app/public/uploads'),
         filename(req,file,callback) {
             const hash = crypto.randomBytes(6).toString('hex');
             const fileName = `${hash}-${file.originalname}`;
